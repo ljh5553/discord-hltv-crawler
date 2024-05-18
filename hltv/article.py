9f21c2cc853ef93f5ff77d88a6757c7f93ca8f06
INZY <ljh5553@gmail.com>
2024-05-18 16:45:12 +0900

    fixed TypeError bug when crawling articles

--- hltv/article.py
+++ hltv/article.py
@@ -4,23 +4,24 @@ from bs4 import BeautifulSoup
 def crawl_article():
     HLTV_MAIN = 'https://hltv.org'
 
-    headers = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"}
-    res = requests.get(HLTV_MAIN, headers=headers)
-    html = res.text
-    soup = BeautifulSoup(html, 'html.parser')
-
-    if soup.find("div", {"class" : "newsgrouping"}): # if there is live update (big events)
-        main_div = soup.find_all("div", {"class" : "standard-box standard-list"})[1]
-    else: # there is no live update (normal situation)
-        main_div = soup.find("div", {"class" : "standard-box standard-list"})
-    link = main_div.find("a").attrs["href"]
-
-    url = HLTV_MAIN + link
-    res = requests.get(url, headers=headers)
-    html = res.text
-    soup = BeautifulSoup(html, 'html.parser')
-
     try:
+        headers = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"}
+        res = requests.get(HLTV_MAIN, headers=headers)
+        html = res.text
+        soup = BeautifulSoup(html, 'html.parser')
+
+        if soup.find("div", {"class" : "newsgrouping"}): # if there is live update (big events)
+            main_div = soup.find_all("div", {"class" : "standard-box standard-list"})[1]
+        else: # there is no live update (normal situation)
+            main_div = soup.find("div", {"class" : "standard-box standard-list"})
+
+        link = main_div.find("a").attrs["href"]
+
+        url = HLTV_MAIN + link
+        res = requests.get(url, headers=headers)
+        html = res.text
+        soup = BeautifulSoup(html, 'html.parser')
+
         article_div = soup.find("article", {"class" : "newsitem standard-box"})
         title = article_div.find("h1", {"class" : "headline"}).text
         header = article_div.find("p", {"class" : "headertext"}).text
